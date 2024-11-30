de6b3d09f1df1cff1b2208b3febda212b9874bf1
patacoing <romain.lefebvre1104@gmail.com>
2024-11-30 11:42:34 +0100

    feat: download latest model if version not provided

--- train/run_download_model.sh
+++ train/run_download_model.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 MODEL_VERSION=$1
 AZURE_RESOURCE_GROUP_NAME=$2
 AZURE_ML_WORKSPACE_NAME=$3
@@ -6,6 +8,14 @@ AZURE_SUBSCRIPTION_ID=$5
 DOWNLOAD_PATH=$6
 MODEL_NAME="degradai"
 
+
+# Retrieve the latest model version if not provided
+if [ "$MODEL_VERSION" = "latest" ]
+then
+  MODEL_VERSION=$(az ml model list --workspace-name "$AZURE_ML_WORKSPACE_NAME" --resource-group "$AZURE_RESOURCE_GROUP_NAME" --name "$MODEL_NAME" --query "max([*].version).to_number(@)")
+fi
+
+echo "Retrieving the model version $MODEL_VERSION"
 
 poetry run python azureml_run_download_model.py \
     --subscription_id "$AZURE_SUBSCRIPTION_ID" \
