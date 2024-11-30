e26d215f0002f25dcae64e741883c18ed1944b38
patacoing <romain.lefebvre1104@gmail.com>
2024-11-30 12:46:31 +0100

    feat: setup credentials
    Added options to the script and saving subscription id

--- setup_credentials.sh
+++ setup_credentials.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
 
+
+function usage {
+  echo "Usage: $0 subscription_id [options]"
+  echo "Options:"
+  echo "  -g: Set the secret in the GitHub repository"
+  echo "  -h: Display this help message"
+  exit 1
+}
+
 SUBSCRIPTION_ID=$1
+shift
+
+SECRET_FILE="act.secrets"
+GH_SECRET=false
+GH_ENV="degradai"
+OPTSTRING="gh"
+
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    g)
+      echo "Setting secret in GitHub repository"
+      GH_SECRET=true
+      ;;
+    h)
+      usage
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      usage
+      ;;
+  esac
+done
+
+if [ -z "$SUBSCRIPTION_ID" ]; then
+  usage
+fi
 
 # get credentials
-credentials=$(az ad sp create-for-rbac --name "mlapp-romain" --role contributor --scopes "/subscriptions/46113ffc-4a75-4334-ad25-40ea09122058" --sdk-auth)
+credentials=$(az ad sp create-for-rbac --name "mlapp-romain" --role contributor --scopes "/subscriptions/$SUBSCRIPTION_ID" --sdk-auth)
 
-# Format credentials
+# Format credentials to remove escape characters and remove first and last character
 formatted_credentials=$(echo $credentials | jq -c @json | tr -d "\\" | sed -r 's/^.{1}//' | sed 's/.$//')
 
-gh secret set AZURE_CREDENTIALS --body "$formatted_credentials" --env "degradai"
-echo "AZURE_CREDENTIALS=$formatted_credentials" > act.secrets
+if [ "$GH_SECRET" = true ]; then
+  gh secret set AZURE_CREDENTIALS --body "$formatted_credentials" --env "$GH_ENV"
+  gh secret set AZURE_SUBSCRIPTION_ID --body "$SUBSCRIPTION_ID" --env "$GH_ENV"
+fi
+
+echo "AZURE_CREDENTIALS=$formatted_credentials" > $SECRET_FILE
+echo "AZURE_SUBSCRIPTION_ID=$SUBSCRIPTION_ID" >> $SECRET_FILE
+echo "Secrets saved in $SECRET_FILE"
