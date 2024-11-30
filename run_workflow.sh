819815e9e8141787e45ad3af7ef3bb282d43a222
patacoing <romain.lefebvre1104@gmail.com>
2024-11-30 12:47:04 +0100

    feat: run workflow script
    Setup credentials before running the workflow

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-JOB=${1:-"ml-pipeline"}
+SUBSCRIPTION_ID=$1
+
+if [ -z "$SUBSCRIPTION_ID" ]
+then
+  echo "Usage: $0 <subscription_id> [job_name]"
+  exit 1
+fi
+
+JOB=${2:-"ml-pipeline"}
 SECRET_FILE="act.secrets"
 DOCKER_IMAGE="ghcr.io/catthehacker/ubuntu:act-latest"
 
@@ -10,6 +18,9 @@ then
   echo "act is not installed. Please install it before running this script."
   exit 1
 fi
+
+#Set up the credentials
+source setup_credentials.sh $SUBSCRIPTION_ID
 
 docker pull $DOCKER_IMAGE
 
